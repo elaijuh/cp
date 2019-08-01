eb6beb7f63839cb0f94bb3a192f0b2666037d224
elaijuh <hjiale@gmail.com>
2019-08-02 03:05:15 +0800

    leetcode 3

---
+++ leetcode/3.longest-substring-without-repeating-characters.cpp
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode id=3 lang=cpp
+ *
+ * [3] Longest Substring Without Repeating Characters
+ */
+
+class Solution {
+ public:
+  int lengthOfLongestSubstring(string s) {
+    unordered_map<char, int> c;
+    int i = 0, j = 0, m = 0;
+    while (i < s.length() && j < s.length()) {
+      auto it = c.find(s[i]);
+      if (it != c.end()) {
+        j = max(j, it->second);
+      }
+      m = max(m, i - j + 1);
+      c[s[i]] = i + 1;
+      i++;
+    }
+    return m;
+  }
+};
--- leetcode/foo.cc
+++ leetcode/foo.cc
@@ -1,17 +1,39 @@
 #include <algorithm>
 #include <iostream>
 #include <list>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
 #include <vector>
-
 using namespace std;
 
+class Solution {
+ public:
+  int lengthOfLongestSubstring(string s) {
+    unordered_map<char, int> c;
+    int j = 0;
+    int max = 0;
+    for (int i = 0; i < s.length(); i++) {
+      auto x = c.find(s[i]);
+      if (x == c.end()) {
+        c.insert({s[i], i});
+        if (c.size() > max) {
+          max = c.size();
+        }
+      } else {
+        int xv = x->second;
+        for (int k = j; k <= xv; k++) c.erase(s[k]);
+        j = xv + 1;
+        i--;
+      }
+    }
+    return max;
+  }
+};
+
 int main() {
-  list<int> a = {1, 2, 3, 4, 5, 11, 7};
-  list<int>::iterator it;
-  it = a.begin();
-  a.insert(++it, 10);
-  auto p = find(a.begin(), a.end(), 7);
-  cout << (p == a.end()) << endl;
+  Solution so = Solution();
+  cout << so.lengthOfLongestSubstring("abcabcbb") << endl;
 
   return 0;
 }
